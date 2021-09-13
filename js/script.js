c94df150e040e0cfa202bddbb088f29c555b66d9
Diogo Oliveira <88461069+diiogo45@users.noreply.github.com>
2021-09-13 16:25:07 -0300

    tela cadastro js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function apertar () {
+
+var name1 = document.getElementById("nome");
+
+var surname = document.getElementById("sobrenome");
+
+var telefone = document.getElementById("numero");
+
+if (name1.value == "") {
+
+alert("Por favor digite seu nome");
+
+} if(surname.value == "") {
+
+alert("por favor,digite seu sobrenome");
+
+return false;
+
+} if (telefone.value== "") {
+
+alert("por favor,digite seu telefone ");
+
+
+}
+
+
+}
+
